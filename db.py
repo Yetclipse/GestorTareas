a290e5f65391b568afcca97750020520429c2f95
Sebastien <seal.seal1406@gmail.com>
2026-02-19 03:32:55 +0000

    Pasos finales (1)

--- db.py
+++ db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-
+#Crear carpeta de database si no existe ya que engine no la crea
 engine = create_engine('sqlite:///database/tareas.db',
 connect_args={'check_same_thread': False})
 
